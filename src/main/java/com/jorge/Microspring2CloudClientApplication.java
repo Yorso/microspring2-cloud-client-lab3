92617ac65e20bdbba7d51294df08dfe81aef287c
Yorso <jorgeguzon@gmail.com>
2017-01-12 17:12:12 +0100

    Profile added

--- src/main/java/com/jorge/Microspring2CloudClientApplication.java
+++ src/main/java/com/jorge/Microspring2CloudClientApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Try: 
  * 		http://localhost:8002/lucky-word  => You should see the lucky word message in your browser
  * 
+ * Si tiene perfil activado (ejemplo: spring.profiles.active: northamerica) irá a su archivo yml correspondiente automáticamente (comosellame-northamerica.yml), si no irá al normal (comosellame.yml).
+ * 
  */
 @SpringBootApplication
 public class Microspring2CloudClientApplication {
