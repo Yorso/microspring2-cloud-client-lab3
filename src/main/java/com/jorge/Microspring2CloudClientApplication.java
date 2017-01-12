8b2a98df670e1d704e919a48060628b62b13e33a
Yorso <jorgeguzon@gmail.com>
2017-01-12 17:14:46 +0100

    Comment added

--- src/main/java/com/jorge/Microspring2CloudClientApplication.java
+++ src/main/java/com/jorge/Microspring2CloudClientApplication.java
@@ -10,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Si tiene perfil activado (ejemplo: spring.profiles.active: northamerica) irá a su archivo yml correspondiente automáticamente (comosellame-northamerica.yml), si no irá al normal (comosellame.yml).
  * 
  */
+/**
+ * What happens if the Config Server is unavailable when the “lucky word” application starts? To mitigate this possibility, it is common to run multiple instances of the config server in different racks / zones behind a load balancer.
+ * What happens if we change a property after client applications have started? The server picks up the changes immediately, but the client does not. Later we will see how Spring Cloud Bus and refresh scope can be used to dynamically propagate changes.
+ *
+ */
 @SpringBootApplication
 public class Microspring2CloudClientApplication {
 
