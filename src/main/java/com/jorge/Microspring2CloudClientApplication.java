0b32be6b53cf5745af466ff747e43270300f5d4e
Yorso <jorgeguzon@gmail.com>
2017-01-12 17:04:23 +0100

    Changes

--- src/main/java/com/jorge/Microspring2CloudClientApplication.java
+++ src/main/java/com/jorge/Microspring2CloudClientApplication.java
@@ -3,10 +3,16 @@ package com.jorge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Try: 
+ * 		http://localhost:8002/lucky-word  => You should see the lucky word message in your browser
+ * 
+ */
 @SpringBootApplication
 public class Microspring2CloudClientApplication {
 
 	public static void main(String[] args) {
+		//System.setProperty("spring.cloud.bootstrap.enabled","false");
 		SpringApplication.run(Microspring2CloudClientApplication.class, args);
 	}
 }
