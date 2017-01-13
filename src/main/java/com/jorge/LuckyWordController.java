f432212ee73191911b282730afbf34b9123e4c29
Yorso <jorgeguzon@gmail.com>
2017-01-13 12:52:13 +0100

    changes

--- src/main/java/com/jorge/LuckyWordController.java
+++ src/main/java/com/jorge/LuckyWordController.java
@@ -4,6 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Try: 
+ * 		http://localhost:8002/lucky-word  => You should see the lucky word message in your browser
+ * 
+ * Si tiene perfil activado (ejemplo: spring.profiles.active: northamerica) irá a su archivo yml correspondiente automáticamente (comosellame-northamerica.yml), si no irá al normal (comosellame.yml).
+ * 
+ */
+
 @RestController
 public class LuckyWordController {
 
